0cc8dff091d68759ce69dd937d8fd3c46e70abc0
MR.IRANI <staroo_2@yahoo.com>
2017-04-25 21:52:12 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,5 @@
--- #Beyond Reborn Robot
--- #@BeyondTeam
+-- #delete Team Robot
+-- #@deleteTeam
 
 tdcli = dofile('./libs/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -83,38 +83,18 @@ function create_config( )
 	    "fun",
 	},
     sudo_users = {
-   157059515,
+   240870611,
    sudo_id
 },
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn V5.0
+    info_text = [[》delete robot V5.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer & Sponser《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@ToOfan ➣ Developer《
-》@CiveY ➣ Developer
-》@K_a_I_i_I_i_n_u_x ➣ Developer《
-》@CliFather ➣ Developer《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@Nero_Dev
-》And Beyond Team Members
-
+》https://github.com/ramindel22/delete
 》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+》@deleteTeam《
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
